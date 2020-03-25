9fe60107a9f21f691299baa6e87d175db9bf08da
张永杰 <zhangyongjie@edaijia-inc.cn>
2020-03-25 19:49:34 +0800

    更换域名

--- src/js/common.js
+++ src/js/common.js
@@ -1,12 +1,12 @@
 layui.define([], function(exports) {
     var Common = function() {
         this.VERSION = '1.0.0';
-        this.API_HOST = 'https://www.d.edipao.cn';
+        this.API_HOST = 'https://api.d.edipao.cn';
         if(location.href.indexOf('test.edipao.cn') > -1){
-            this.API_HOST = location.protocol + '//wwwv2.edipao.cn';
+            this.API_HOST = location.protocol + '//api.edipao.cn';
         }
         if(location.href.indexOf('wwwv2.edipao.cn') > -1){
-            this.API_HOST = location.protocol + '//wwwv2.edipao.cn';
+            this.API_HOST = location.protocol + '//api.edipao.cn';
         }
         if(location.href.indexOf('localhost') > -1){
             this.API_HOST = 'http://localhost:8080';
