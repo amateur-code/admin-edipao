b8f9723e8b13ebbad10c4640019d70f546d6337f
张永杰 <zhangyongjie@edaijia-inc.cn>
2020-05-22 18:04:28 +0800

    cs

--- src/orderMessage/vehicleDamage/js/add.js
+++ src/orderMessage/vehicleDamage/js/add.js
@@ -3,9 +3,9 @@ layui.use(['jquery','form', 'layer', 'laytpl', 'table', 'upload'], function(){
       form = layui.form,
       layer = layui.layer,
       laytpl = layui.laytpl,
-      table = layui.table,
       upload = layui.upload,
       edipao = layui.edipao;
+  window.form = form;
   function Add(){
     var qs = edipao.urlGet();
     this.id = qs.id;
@@ -14,7 +14,24 @@ layui.use(['jquery','form', 'layer', 'laytpl', 'table', 'upload'], function(){
   }
   Add.prototype.init = function () {
     var _this = this;
-    this.renderUpload();
+    this.verifyForm();
+    if(_this.action == "edit"){
+
+    }else{
+      this.renderUpload();
+      this.bindEvents();
+    }
+  }
+  Add.prototype.bindEvents = function () {
+    form.on("submit(submit)", function (data) {
+      data = data.field;
+      console.log(data)
+      return false;
+    });
+    $('#addCancel').click(function () {
+      xadmin.close();
+      return false;
+    });
   }
   Add.prototype.renderUpload = function () {
     var _this = this;
@@ -41,7 +58,38 @@ layui.use(['jquery','form', 'layer', 'laytpl', 'table', 'upload'], function(){
     })
   }
   Add.prototype.renderUploadPic = function () {
-    
+    var _this = this;
+    laytpl($("#upload_img_tpl").html()).render({list: _this.picList}, function (html) {
+      $(".upload_pic_item").remove();
+      $("#upload_pic_list").append(html);
+      _this.bindDeletePic();
+      zoomImg();
+    });
+  }
+  Add.prototype.bindDeletePic = function () {
+    var _this = this;
+    $(".pic_del_btn").unbind().on("click", function (e) {
+      var index = e.target.dataset.index * 1;
+      _this.picList.splice(index, 1);
+      _this.renderUploadPic();
+    });
+  }
+  Add.prototype.verifyForm = function () {
+    form.verify({
+      nameVerify: function(value){
+        if(value == undefined){
+          return "必填项不能为空";
+        }
+        if(value == ""){
+          return "必填项不能为空";
+        }
+      },
+      remarkVerify: function (value) {
+        if(value.length > 300){
+          return "备注长度不能超过300个字符";
+        }
+      }
+    })
   }
   var add = new Add();
   add.init();
