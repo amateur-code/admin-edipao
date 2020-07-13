ab394b71937446a0f5735c314ae28265c81e5f53
张永杰 <zhangyongjie@edaijia-inc.cn>
2020-07-13 11:11:47 +0800

    cs

--- src/orderMessage/dispatchManagement/js/rob-edit.js
+++ src/orderMessage/dispatchManagement/js/rob-edit.js
@@ -16,10 +16,18 @@ layui.use(["jquery", "layer", "form", "laytpl", "laydate"], function () {
   }
   Edit.prototype.init = function(){
     var _this = this;
+    _this.renderTime(0);
     _this.bindEvents();
   }
   Edit.prototype.bindEvents = function () {
     var _this = this;
+    $("#time_container").unbind().on("click", function(e){
+      if(e.target.classList.contains("delete_btn")){
+        var index = e.target.dataset.index;
+        $(".time_row_" + index).remove();
+        _this.timeList.splice(index, 1);
+      }
+    });
     $("#add_btn").unbind().on("click", function (e) {
       var index = _this.timeList.length;
       laytpl($("#formTpl").html()).render({
@@ -28,6 +36,13 @@ layui.use(["jquery", "layer", "form", "laytpl", "laydate"], function () {
         $("#time_container").append(html);
         _this.renderTime(index);
       });
+    });
+    $("#add_cancel").unbind().click(function () {
+      xadmin.close();
+      return false;
+    });
+    form.on("submit(main_form)", function (data) {
+      return false;
     });
   }
   Edit.prototype.renderTime = function (index) {
--- src/orderMessage/dispatchManagement/js/rob-setting.js
+++ src/orderMessage/dispatchManagement/js/rob-setting.js
@@ -1,8 +1,16 @@
-layui.use(["jquery", "layer"], function ($, layer) {
-  var edipao = layui.edipao;
+layui.use(["jquery", "layer"], function () {
+  var edipao = layui.edipao,
+    $ = layui.jquery,
+    layer = layui.layer;
   function View() {  }
   View.prototype.init = function(){
-
+    var _this = this;
+    _this.bindEvents();
+  }
+  View.prototype.bindEvents = function () {
+    $("#edit_btn").unbind().click(function () {
+      xadmin.open("抢单配置修改", "./rob-edit.html");
+    });
   }
   var view = new View();
   view.init();
